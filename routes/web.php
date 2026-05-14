b26d1d6d7ae34f561096d6bee4dfea7855d7aa50
MatheusMottaGit <matheusprofess@gmail.com>
2026-05-14 01:34:12 -0300

    corrigindo rotas

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,11 @@ use App\Http\Controllers\PredictionController;
 use App\Http\Controllers\RankingController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome')->name('home');
+Route::get('/', function () {
+    return auth()->check()
+        ? redirect()->route('groups.index')
+        : redirect()->route('login');
+})->name('home');
 
 Route::middleware(['auth'])->group(function () {
     Route::redirect('dashboard', '/groups')->name('dashboard');
